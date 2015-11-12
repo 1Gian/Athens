13ee8c7a3bb3df650b99bf841f4e2d68727b8021
1Gian <giansemi@gmail.com>
2015-11-12 18:48:19 +0300

    Initial commit

---
+++ app/src/main/java/com/gian/athens/splash.java
@@ -0,0 +1,14 @@
+package com.gian.athens;
+
+import android.app.Activity;
+
+/**
+ * Created by gian on 11/12/15.
+ */
+public class splash extends Activity {
+
+
+        @Override
+         protected void
+
+}
